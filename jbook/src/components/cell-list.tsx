91e2a5d78aed5169d837100060947d6b89760680
Kabooley <35451559+Kabooley@users.noreply.github.com>
2023-02-28 21:04:57 +0900

    lecture 231

--- jbook/src/components/cell-list.tsx
+++ jbook/src/components/cell-list.tsx
@@ -1,15 +1,29 @@
-
+import { Fragment } from 'react';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import CellListItem from './cell-list-item';
+import AddCell from './add-cell';
 
 const CellList: React.FC = () => {
     const cells = useTypedSelector(({ cells: { order, data }}) => order.map((id) => data[id]));
 
-    const renderedCells = cells.map(cell => <CellListItem cell={cell} />);
+    const renderedCells = cells.map(
+        cell => (
+        <Fragment key={cell.id} >
+            <AddCell nextCellId={cell.id} />
+            <CellListItem cell={cell} />
+        </Fragment>);
+    );
 
     console.log(renderedCells);
 
-    return <div>{renderedCells}</div>;
+    return (
+        <div>
+            {renderedCells}
+            <div className={CellList.length === 0 ? 'force-visibile': ''}>
+                <AddCell nextCellId={null} />
+            </div>
+        </div>
+    );
 };
 
 export default CellList;
