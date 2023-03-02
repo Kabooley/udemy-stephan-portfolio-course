da2e9679cdfd8f46221294eee6b9faeab73a5c73
Kabooley <35451559+Kabooley@users.noreply.github.com>
2023-03-03 01:06:50 +0900

    lecture 244

--- jbook/src/state/reducers/bundlesReducer.ts
+++ jbook/src/state/reducers/bundlesReducer.ts
@@ -4,9 +4,8 @@ import { Action } from '../actions';
 
 interface BundleState {
     [key: string]: {
-        // BUNDLE_STARTのたびにtrue、BUNDLE_COMPLETEのたびにfalse
-        loading: boolean;
         code: string;
+        loading: boolean;
         err: string;
     }
 };
@@ -16,12 +15,29 @@ const initialState: BundleState = {};
 const reducer = produce((state: BundleState = initialState, action: Action): BundleState => {
     switch(action.type) {
         case ActionType.BUNDLE_START:
+            state[action.payload.cellId] = {
+                // NOTE: バンドリングプロセスが開始なのでtrue
+                loading: true,
+                // 初期値
+                code: '',
+                err: ''
+            };
             return state;
         case ActionType.BUNDLE_COMPLETE:
+            state[action.payload.cellId] = {
+                // NOTE: バンドリングプロセスが終了なのでfalse
+                loading: false,
+                // バンドル結果はディスパッチで与えられる
+                code: action.payload.bundle.code,
+                err: action.payload.bundle.err
+            };
             return state;
-        default:
+        default: 
             return state;
-    }
-});
+    };
+},
+    // NOTE: To avoid error
+    initialState
+);
 
 export default reducer;
